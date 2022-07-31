54d137fa1148c265910d137f2453368128959e2e
Ilia_Khrebtov <Ilia_Khrebtov@epam.com>
2022-07-31 18:44:05 +0500

    integrate import endpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   product: 'https://jerjpx32k5.execute-api.eu-central-1.amazonaws.com/dev/product',
   products: 'https://jerjpx32k5.execute-api.eu-central-1.amazonaws.com/dev/products',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://l85dhjct8g.execute-api.eu-central-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
